631fb85fdb9fa4a74182626a05914a79cc5cd7ef
David Moothen <dmoothen@websitti.fr>
2015-11-30 22:46:26 +0100

    Create sidebar plugin.

--- src/js/jquery.sidebar.js
+++ src/js/jquery.sidebar.js
@@ -3,7 +3,8 @@
     $.fn.sidebar = function(params){
 
         var defaults = {
-
+            trigger: '.toggle-sidebar',
+            duration: 500
         };
 
         params = $.extend({}, defaults, params);
@@ -11,6 +12,47 @@
         return this.each(function(){
 
             var $element = $(this);
+            var $window = $(window);
+            var $wrapper = $('#wrapper');
+            var $main = $('#main');
+            var $sidebar = $('#sidebar');
+            var open = false;
+
+            var outerClick = function(e){
+                e.preventDefault();
+                closeSidebar();
+            };
+
+            var openSidebar = function(e){
+                $wrapper.animate({
+                    left: '300px'
+                }, params.duration, function(){
+                    open = true;
+                    $main.bind('click', outerClick);
+                    $('a:not(.no-close)', $sidebar).bind('click', closeSidebar);
+                });
+            };
+
+            var closeSidebar = function(e){
+                $wrapper.animate({
+                    left: '0px'
+                }, params.duration, function(){
+                    open = false;
+                    $main.unbind('click', outerClick);
+                    $('a:not(.no-close)', $sidebar).unbind('click', closeSidebar);
+                });
+            };
+
+            var toggleSidebar = function(e){
+                e.preventDefault();
+                if (open) {
+                    closeSidebar(e);
+                } else {
+                    openSidebar(e);
+                }
+            };
+
+            $(params.trigger).click(toggleSidebar);
 
         });
 
