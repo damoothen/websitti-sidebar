273f580d44cfebcba9574da285649ee700ff49b8
David Moothen <dmoothen@websitti.fr>
2015-11-30 17:35:10 +0100

    Init project.

---
+++ src/js/jquery.sidebar.js
@@ -0,0 +1,19 @@
+(function($){
+
+    $.fn.sidebar = function(params){
+
+        var defaults = {
+
+        };
+
+        params = $.extend({}, defaults, params);
+
+        return this.each(function(){
+
+            var $element = $(this);
+
+        });
+
+    };
+
+})(jQuery);
